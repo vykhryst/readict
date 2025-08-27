e34df4a7fe9568cd91d1734aa98190351154cdb6
Oksana Vykhryst <oksana.vykhryst@nure.ua>
2025-08-27 14:19:01 +0300

    Remove commented code

--- frontend/src/hooks/useBookCard.js
+++ frontend/src/hooks/useBookCard.js
@@ -4,10 +4,8 @@ import {useCallback, useMemo} from "react";
 export function useBookCard(book) {
     const navigate = useNavigate();
 
-    // округлений рейтинг
     const rating = useMemo(() => Math.round(book.averageRating * 2) / 2, [book.averageRating]);
 
-    // функція рендера зірочок
     const renderStar = useCallback(
         (i) => {
             if (i + 1 <= rating) return "fas fa-star";
@@ -17,7 +15,6 @@ export function useBookCard(book) {
         [rating]
     );
 
-    // обробка кліку на картку
     const handleCardClick = useCallback(
         (e) => {
             if (e.target.closest("a, button")) return;
